385a9f76a15687282fb474868a9ebb52bad5ef10
Jeffrey Chuc <jeffreychuc@gmail.com>
2018-02-12 22:34:08 -0800

    add comments

--- n_primes_below.js
+++ n_primes_below.js
@@ -1,17 +1,22 @@
 const nPrimes = (n) => {
+  // generate array with values of 0 -> n
   let ret = Array.from({length: n+1}, (v, k) => k);
-  console.log(ret);
+  // console.log(ret);
+  // only need to check up to math.sqrt(n)
   let maxCheck = Math.sqrt(n);
+
   for (let i = 2; i <= maxCheck; i++) {
+    // if value is true, set all multiples to false
     if (ret[i]) {
       for (let j = i+i; j < n; j+=i) {
         ret[j] = false;
       }
     }
   }
+  // remove 0, 1
   ret.shift();
   ret.shift();
-  ret = ret.filter((val) => val);
-  console.log(ret);
+  // filter out false values
+  return ret.filter((val) => val);
 }
-console.log(nPrimes(1000000000));
+console.log(nPrimes(1000000));
